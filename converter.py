89ab7c5a3e19c2e62bb88129eb5ff79131ba5d99
Harold Penaloza <harold@ubidots.com>
2021-10-30 02:11:05 -0500

    :children_crossing: feat: round value

--- converter.py
+++ converter.py
@@ -2,5 +2,6 @@ pesos = input("Cuantos pesos colombianos tienes?: ")
 pesos = float(pesos)
 valor_dolar = 3766
 dolares = pesos / valor_dolar
+dolares = round(dolares, 2)
 dolares = str(dolares)
 print("Tienes $" + dolares + " dolares")
