3737b4a43e329c890886816302b2deaf183fbf68
Harold Penaloza <harold@ubidots.com>
2021-10-30 03:05:23 -0500

    :sparkles: feat: new coins support

--- converter_to_dolar.py
+++ converter_to_dolar.py
@@ -1,7 +1,35 @@
-pesos = input("Cuantos pesos colombianos tienes?: ")
-pesos = float(pesos)
-valor_dolar = 3766
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $" + dolares + " dolares")
+menu = """
+Bienvenido al conversor de monedas.
+
+1 - Pesos colombianos
+2 - Pesos argentinos
+3 - Pesos mexicanos
+
+Elige una opción: """
+
+opcion = int(input(menu))
+
+if opcion == 1:
+  pesos = float(input("¿Cuántos pesos colombianos tienes?: "))
+  valor_dolar = 3766
+  dolares = round(pesos / valor_dolar, 2)
+  dolares = str(dolares)
+  print("Tienes $" + dolares + " dólares")
+elif opcion == 2:
+  pesos = float(input("¿Cuántos pesos argentinos tienes?: "))
+  valor_dolar = 65
+  dolares = round(pesos / valor_dolar, 2)
+  dolares = str(dolares)
+  print("Tienes $" + dolares + " dólares")
+elif opcion == 3:
+  pesos = float(input("¿Cuántos pesos mexicanos tienes?: "))
+  valor_dolar = 24
+  dolares = round(pesos / valor_dolar, 2)
+  dolares = str(dolares)
+  print("Tienes $" + dolares + " dólares")
+else:
+  print("Opción inválida")
+
+
+
+
